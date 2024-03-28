5982a2c2add74b82d971c76522b1f5f918f7bf68
Duotech B4 <duotechb3@gmail.com>
2024-03-28 19:07:28 -0400

    types of hooks, tagged hooks, pretty plugin, step notifications

--- src/test/java/runner/CucumberRunner.java
+++ src/test/java/runner/CucumberRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 @CucumberOptions (
 
-        tags = "@REGRESSION",
+        tags = "@smoke or @flaky",
         //"@smoke and @flaky" - scenarios tagged with both smoke and flaky
         //"@smoke or @flaky" - scenarios tagged with either smoke or flaky
         //"not @REGRESSION" - scenarios not tagged with regression
@@ -15,10 +15,11 @@ import org.junit.runner.RunWith;
         features = "src/test/resources", // path to the location of the feature files
         glue = "stepDefinitions", // path to the location of step definition classes
         plugin = {
+             "pretty",   // adds more detailed info and logs on the console for debugging
              "html:target/cucumber-report/report.html" // generates a local html report
         },
-        publish = true // generated a web based html report with link on the console
-
+        publish = true, // generates a web based html report with link on the console
+        stepNotifications = true
 //        ,dryRun = true  // dry runs the scenario for snippet generation
 )
 @RunWith(Cucumber.class)
--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,8 +1,6 @@
 package stepDefinitions;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
+import io.cucumber.java.*;
 import io.cucumber.java.en.Then;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -14,11 +12,20 @@ import java.time.Duration;
 public class Hooks {
 
 
-    @Before
+    @Before ()
     public void setupScenario(){
         Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         Driver.getDriver().manage().window().maximize();
     }
+
+    //Tagged Hook example
+//    @Before ("@flaky or @failing")
+//    public void setupScenario2(){
+//        System.out.println("Hook for Flaky");
+//        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+//        Driver.getDriver().manage().window().maximize();
+//        Driver.getDriver().manage().deleteAllCookies();
+//    }
 
     @After
     public void tearDownScenario(Scenario scenario){
@@ -28,6 +35,29 @@ public class Hooks {
 
         Driver.quitDriver();
     }
+
+
+//    @BeforeStep
+//    public void setStep(){
+//        System.out.println("Before Step");
+//    }
+
+//    @AfterStep ("@homepage") // tagged Hook
+//    public void afterStep(){
+//        System.out.println("After Step");
+//    }
+
+
+//    @BeforeAll
+//    public static void setupAll(){
+//        System.out.println("Before All Scenarios");
+//    }
+//
+//    @AfterAll
+//    public static void tearDownAll(){
+//        System.out.println("After All Scenarios");
+//
+//    }
 
 
 }
