63000362fdacba1a83d855ee3207b73de30fdb8c
Duotech B4 <duotechb3@gmail.com>
2024-04-27 12:20:58 -0400

    serialization and pojos

---
+++ src/test/java/apiTestsTDD/RestAssuredDeserialization.java
@@ -0,0 +1,4 @@
+package apiTestsTDD;
+
+public class RestAssuredDeserialization {
+}
---
+++ src/test/java/pojo/User.java
@@ -0,0 +1,30 @@
+package pojo;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import io.cucumber.java.mk_latn.No;
+import io.cucumber.java.sl.In;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Data //
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder  // Allows object creation using builder pattern
+@JsonInclude(Include.NON_NULL) // allows null fields to be ignored when object is serialized to json
+public class User {
+
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String signUpDate;
+    private String profilePic;
+
+
+}
